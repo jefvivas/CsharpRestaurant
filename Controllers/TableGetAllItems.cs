01291368ced326c9ee3cbd9783c18a1da0cd8db1
Jeferson <jefvivasaragao@gmail.com>
2023-09-24 13:03:49 -0300

    create getAllItems from table route

---
+++ Controllers/TableGetAllItems.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Restaurant.Models;
+using Restaurant.Services;
+
+namespace Restaurant.Controllers;
+
+[Authorize]
+[ApiController]
+[Route("consume")]
+public class TableGetAllItems : ControllerBase
+{
+    private readonly TableServices _tableServices;
+    private readonly JwtServices _jwtService;
+
+
+    public TableGetAllItems(TableServices tableServices, JwtServices jwtService)
+    {
+        _tableServices = tableServices;
+        _jwtService = jwtService;
+
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var uniqueNameClaim = _jwtService.GetUniqueNameClaim(HttpContext);
+
+        if (string.IsNullOrWhiteSpace(uniqueNameClaim))
+        {
+            return BadRequest("Invalid Token.");
+        }
+
+        var tableData = await _tableServices.GetTableByNumber(uniqueNameClaim);
+
+        var response = new TableAllItemsModel
+        {
+            Items = tableData.ConsumedProducts.Select(p => new ProductItem
+            {
+                ProductId = p.Key,
+                Quantity = p.Value
+            }).ToList()
+        };
+
+        return Ok(response);
+    }
+}
---
+++ Models/TableAllItemsModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Restaurant.Models;
+
+public class TableAllItemsModel
+{
+    public List<ProductItem> Items { get; set; }
+
+    public TableAllItemsModel()
+    {
+        Items = new List<ProductItem>();
+    }
+}
